d92154daa2b2063192324fecfb28d433571e7ee6
shinopai <harbor.shinozaki@gmail.com>
2026-05-20 06:53:13 +0000

    feat: ユーザー登録時に初期カテゴリーとサブカテゴリーを複製

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,27 @@ class User < ApplicationRecord
   # リレーション
   has_many :categories, dependent: :destroy
   has_many :sub_categories, dependent: :destroy
+
+  # コールバック
+  after_create :copy_default_categories, unless: :admin?
+
+  private
+
+def copy_default_categories
+  admin_user = User.find_by(admin: true)
+  return unless admin_user
+
+  admin_user.categories.includes(:sub_categories).find_each do |admin_category|
+    new_category = categories.create!(
+      name: admin_category.name
+    )
+
+    admin_category.sub_categories.each do |admin_sub_category|
+      sub_categories.create!(
+        category: new_category,
+        name: admin_sub_category.name
+      )
+    end
+  end
+end
 end
