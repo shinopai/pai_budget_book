b44346c08aa61d6eb4bb64fd56554c22350c5984
shinopai <harbor.shinozaki@gmail.com>
2026-05-20 01:34:01 +0000

    feat: 管理者サブカテゴリーの作成・編集フォームを追加

--- app/controllers/admin/sub_categories_controller.rb
+++ app/controllers/admin/sub_categories_controller.rb
@@ -4,4 +4,40 @@ class Admin::SubCategoriesController < ApplicationController
                                 .includes(:category)
                                 .order(:id)
   end
+
+  def new
+  @sub_category = current_user.sub_categories.new
+end
+
+def create
+  @sub_category = current_user.sub_categories.new(sub_category_params)
+
+  if @sub_category.save
+    redirect_to admin_sub_categories_path,
+                notice: "サブカテゴリーを作成しました。"
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+def edit
+  @sub_category = current_user.sub_categories.find(params[:id])
+end
+
+def update
+  @sub_category = current_user.sub_categories.find(params[:id])
+
+  if @sub_category.update(sub_category_params)
+    redirect_to admin_sub_categories_path,
+                notice: "サブカテゴリーを更新しました。"
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+private
+
+def sub_category_params
+  params.require(:sub_category).permit(:category_id, :name)
+end
 end
