ced46548b354fcdf07898e46003a38e839e06e8e
shinopai <harbor.shinozaki@gmail.com>
2026-05-20 00:59:21 +0000

    feat: 管理者サブカテゴリー一覧画面を追加

--- app/controllers/admin/sub_categories_controller.rb
+++ app/controllers/admin/sub_categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::SubCategoriesController < ApplicationController
   def index
-
+  @sub_categories = current_user.sub_categories
+                                .includes(:category)
+                                .order(:id)
   end
 end
